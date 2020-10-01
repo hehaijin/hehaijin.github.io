13ae91c2a9cf1ec6921cd21f6953b501b0fbdf16
hehaijin <hehaijin1981@gmail.com>
2020-10-01 13:17:56 -0600

    update 1

---
+++ precache-manifest.37a08bdf0ebdd9d79d2616934bfdbe9c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "382209b501b0a866897abc183a17ab6c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ab53a9357e925c187b23",
+    "url": "/static/css/main.b89973a7.chunk.css"
+  },
+  {
+    "revision": "ff317e0d9dba3e37aef4",
+    "url": "/static/js/2.f7ac99c0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f7ac99c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab53a9357e925c187b23",
+    "url": "/static/js/main.455d95c8.chunk.js"
+  },
+  {
+    "revision": "4fd82a4f8edcdb2135eb",
+    "url": "/static/js/runtime-main.b0a26fe8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
